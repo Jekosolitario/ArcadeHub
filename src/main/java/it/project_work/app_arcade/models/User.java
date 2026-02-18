ee3690e70949485461f2bd66458a18f4fe4e308e
yuchensu <3112787849suyuchen@gmail.com>
2026-02-18 09:19:25 +0100

    messo GlobalExceptionHandler

--- src/main/java/it/project_work/app_arcade/models/User.java
+++ src/main/java/it/project_work/app_arcade/models/User.java
@@ -2,7 +2,6 @@ package it.project_work.app_arcade.models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
