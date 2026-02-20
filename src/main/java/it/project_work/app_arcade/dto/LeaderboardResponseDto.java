9cdebcba5636f22e393d7de81805b1e8c0893705
Gilda Raccanelli <tua.email@esempio.com>
2026-02-20 12:54:13 +0100

    aggiunti a LeaderboardResponse campi per level, gameCount

--- src/main/java/it/project_work/app_arcade/dto/LeaderboardResponseDto.java
+++ src/main/java/it/project_work/app_arcade/dto/LeaderboardResponseDto.java
@@ -4,5 +4,7 @@ import java.util.List;
 
 public record LeaderboardResponseDto(
     List<LeaderboardResponse> rows,
-    String gameCode
+    String gameCode,
+    Integer level,
+    Integer gameCount
 ) {}
